547c5ca83f15f916f744a5f80a713aa5de5c9a44
Alexandr <Akeley1994@gmail.com>
2020-06-06 18:19:26 +0300

    create static index

--- index.js
+++ index.js
@@ -13,24 +13,24 @@ app.use('/api/auth', require('./Routes/auth'));
 app.use('/api/data', require('./Routes/data'));
 app.use('/api/item', require('./Routes/item'));
 
-// app.get('/download', (req, res) => {
-//   const file = `${__dirname}/Downloads/LOGOtip-app-release.apk`;
+app.get('/download', (req, res) => {
+  const file = `${__dirname}/Downloads/LOGOtip-app-release.apk`;
 
-//   const filename = path.basename(file);
-//   const mimetype = mime.lookup(file);
+  const filename = path.basename(file);
+  const mimetype = mime.lookup(file);
 
-//   res.setHeader('Content-disposition', `attachment; filename=${filename}`);
-//   res.setHeader('Content-type', mimetype);
+  res.setHeader('Content-disposition', `attachment; filename=${filename}`);
+  res.setHeader('Content-type', mimetype);
 
-//   const filestream = fs.createReadStream(file);
-//   filestream.pipe(res);
-// });
+  const filestream = fs.createReadStream(file);
+  filestream.pipe(res);
+});
 
-// app.get('/', express.static(path.join(__dirname, 'Public')));
+app.use('/', express.static(path.join(__dirname, 'Public')));
 
-// // app.get('/', (req, res) => {
-// //   res.sendFile(path.resolve(__dirname, 'Public', 'index.html'));
-// // });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'Public', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`server up and started on port:${PORT}`);

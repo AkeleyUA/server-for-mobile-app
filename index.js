67d171438f87ea9e70274b45d40077dfa73a6ff4
Alexandr <Akeley1994@gmail.com>
2020-06-06 16:01:29 +0300

    create static index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dbConnect = require('./db')();
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,8 @@ app.use(express.json({ extended: true }));
 app.use('/api/auth', require('./Routes/auth'));
 app.use('/api/data', require('./Routes/data'));
 app.use('/api/item', require('./Routes/item'));
+
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`server up and started on port:${PORT}`);

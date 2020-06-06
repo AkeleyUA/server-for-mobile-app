ca1b62a5d381e86397251e0f91996be4a4ed6df5
Alexandr <Akeley1994@gmail.com>
2020-06-06 16:07:08 +0300

    create static index

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use('/api/item', require('./Routes/item'));
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`server up and started on port:${PORT}`);
 });

fa38011f2122403d899f51fe0d0ba9b9c416d214
Alexandr Stepaniuk <Akeley1994@gmail.com>
2020-07-06 22:02:36 +0300

    fix download file name

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-const express = require('express');
-const path = require('path');
-const mime = require('mime-types');
-const fs = require('fs');
-const dbConnect = require('./db')();
+const express = require("express");
+const path = require("path");
+const mime = require("mime-types");
+const fs = require("fs");
+const dbConnect = require("./db")();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.use(express.json({ extended: true }));
-app.use('/api/auth', require('./Routes/auth'));
-app.use('/api/data', require('./Routes/data'));
-app.use('/api/item', require('./Routes/item'));
+app.use("/api/auth", require("./Routes/auth"));
+app.use("/api/data", require("./Routes/data"));
+app.use("/api/item", require("./Routes/item"));
 
-app.get('/download', (req, res) => {
-  const file = `${__dirname}/Downloads/LOGOtip-app-release.apk`;
+app.get("/download", (req, res) => {
+  const file = `${__dirname}/Downloads/app-release.apk`;
 
   const filename = path.basename(file);
   const mimetype = mime.lookup(file);
 
-  res.setHeader('Content-disposition', `attachment; filename=${filename}`);
-  res.setHeader('Content-type', mimetype);
+  res.setHeader("Content-disposition", `attachment; filename=${filename}`);
+  res.setHeader("Content-type", mimetype);
 
   const filestream = fs.createReadStream(file);
   filestream.pipe(res);
 });
 
-app.use('/', express.static(path.join(__dirname, 'Public')));
+app.use("/", express.static(path.join(__dirname, "Public")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'Public', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "Public", "index.html"));
 });
 
 app.listen(PORT, () => {
